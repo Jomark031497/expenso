94e1fe3451417d99a74cd4b93de66e5ef6bbb3c4
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-09-05 17:48:18 +0800

    added logger

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,14 @@
 import express from "express";
+import { logger } from "./utils/logger";
 
 const main = async () => {
   const app = express();
 
   app.listen(process.env.PORT, () => {
-    console.log(`Server started at http://localhost:${process.env.PORT}`);
+    logger.info(`Server started at http://localhost:${process.env.PORT}`);
   });
 };
 
 main().catch((err) => {
-  console.log(err);
+  logger.error(err);
 });
---
+++ api/src/utils/logger.ts
@@ -0,0 +1,18 @@
+import { format } from "date-fns";
+import { pino } from "pino";
+
+export const logger = pino({
+  base: {
+    pid: false,
+  },
+  level: process.env.NODE_ENV === "test" ? "silent" : "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+
+  timestamp: () =>
+    `,"time":"${format(new Date(), "hh:mm:ss aa | MMM-dd-yyyy")}"`,
+});
