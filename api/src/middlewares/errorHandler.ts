bea44b068b6a9f2758582cf99e71651ea0cac311
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-09-07 19:35:15 +0800

    fea: improve error handler flow and logging

--- api/src/middlewares/errorHandler.ts
+++ api/src/middlewares/errorHandler.ts
@@ -3,16 +3,22 @@ import { AppError } from "../utils/appError.js";
 import { logger } from "../utils/logger.js";
 
 export const errorHandler = (error: AppError | Error, _req: Request, res: Response, next: NextFunction) => {
-  if (!error) next();
+  if (!error) return next();
 
-  logger.error(error);
+  logger.error({
+    message: error.message,
+    stack: error.stack,
+    ...(error instanceof AppError ? { statusCode: error.statusCode, errors: error.errors } : {}),
+  });
 
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       message: error.message,
-      errors: error.errors,
+      errors: error.errors || null,
     });
   }
 
-  return res.status(500).json({ message: "Something went wrong" });
+  return res.status(500).json({
+    message: "Something went wrong",
+  });
 };
