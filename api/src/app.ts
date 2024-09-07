836c03f749ff609166e2568ad05862a6a1c5d7ba
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-09-07 21:19:41 +0800

    feat: added cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,9 +2,19 @@ import express from "express";
 import { initializeRoutes } from "./routes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import type { Session, User } from "lucia";
+import cors from "cors";
+import { env } from "./config/env.js";
 
 export const createApp = () => {
   const app = express();
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: env.CLIENT_URL,
+      methods: ["GET", "POST", "PATCH", "DELETE"],
+    }),
+  );
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
