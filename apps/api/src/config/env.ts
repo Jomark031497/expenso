c25d8467fcc968c96d539e40b3adde5cad079970
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-09-12 19:11:52 +0800

    setup pnpm workspace

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -6,9 +6,6 @@ const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   CLIENT_URL: z.string().url(),
   BASE_URL: z.string().url(),
-  DISCORD_CLIENT_ID: z.string(),
-  DISCORD_CLIENT_SECRET: z.string(),
-  COOKIE_NAME: z.string(),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
