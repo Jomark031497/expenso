a86ebe257c76d61009f8ccbce8d94a57837da2f2
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-09-16 23:44:21 +0800

    chore: creating navbar

--- apps/web/src/features/auth/routes/Login.tsx
+++ apps/web/src/features/auth/routes/Login.tsx
@@ -67,7 +67,7 @@ export const Login = () => {
           Forgot your Password?
         </Link>
 
-        <Button type="submit" className="border-2 border-transparent bg-primary text-white hover:bg-primary/95">
+        <Button type="submit" className="bg-primary hover:bg-primary/95 border-2 border-transparent text-white">
           Login
         </Button>
 
