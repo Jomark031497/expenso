f410ebf5dc06f1cb236cc23714dc89b01d29c0c8
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-09-24 02:36:06 +0800

    chore: wallet creation

--- apps/web/src/components/layouts/Sidebar.tsx
+++ apps/web/src/components/layouts/Sidebar.tsx
@@ -13,7 +13,7 @@ export const Sidebar = ({ isOpen, close }: SidebarProps) => {
   return (
     <aside
       className={clsx(
-        "fixed left-0 top-0 z-30 h-screen w-80 overflow-y-auto bg-white transition-transform duration-500 md:relative md:translate-x-0 md:border-r",
+        "fixed left-0 top-0 z-30 h-screen w-80 overflow-y-auto bg-white transition-transform duration-500 md:relative md:z-0 md:translate-x-0 md:border-r",
         isOpen ? "" : "-translate-x-full md:translate-x-0",
       )}
     >
