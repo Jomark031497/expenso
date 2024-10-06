9a8afc74716fbbae14c6047142c11adb6920ad54
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-10-07 01:32:42 +0800

    chore: implementing suspense

--- web/src/features/dashboard/routes/Dashboard.tsx
+++ web/src/features/dashboard/routes/Dashboard.tsx
@@ -2,8 +2,12 @@ import { useAuth } from "@/features/auth/hooks/useAuth";
 import { useWallets } from "@/features/wallets/hooks/useWallets";
 import { useTransactions } from "@/features/transactions/hooks/useTransactions";
 import { usePagination } from "@/features/misc/hooks/usePagination";
-import { RecentTransactions } from "@/features/transactions/components/RecentTransactions";
-import { WalletsList } from "@/features/wallets/components/WalletsList";
+import { lazily } from "react-lazily";
+import { Suspense } from "react";
+import { Navigate } from "react-router-dom";
+
+const { WalletsList } = lazily(() => import("@/features/wallets/components/WalletsList"));
+const { RecentTransactions } = lazily(() => import("@/features/transactions/components/RecentTransactions"));
 
 export const Dashboard = () => {
   const { user } = useAuth();
@@ -12,18 +16,22 @@ export const Dashboard = () => {
   const { pagination, onPaginationChange } = usePagination();
   const { data: transactions } = useTransactions(pagination);
 
-  if (!user) return <>No user</>;
+  if (!user) return <Navigate to="/auth/login" />;
 
   return (
     <div className="flex flex-col gap-8">
-      <WalletsList userId={user.id} wallets={wallets} />
+      <Suspense fallback={<>Loading Wallets...</>}>
+        <WalletsList userId={user.id} wallets={wallets} />
+      </Suspense>
 
-      <RecentTransactions
-        transactions={transactions}
-        userId={user.id}
-        pagination={pagination}
-        onPaginationChange={onPaginationChange}
-      />
+      <Suspense fallback={<>Loading Transactions...</>}>
+        <RecentTransactions
+          transactions={transactions}
+          userId={user.id}
+          pagination={pagination}
+          onPaginationChange={onPaginationChange}
+        />
+      </Suspense>
     </div>
   );
 };
