e524a68ab304139039a0e68e41448f2c78cc5706
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-10-07 00:41:33 +0800

    refactor(web): moved fonts to AppProvider

--- web/src/features/misc/components/AppProvider.tsx
+++ web/src/features/misc/components/AppProvider.tsx
@@ -2,6 +2,11 @@ import { queryClient } from "@/lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { ReactNode } from "react";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 
 interface AppProviderProps {
   children: ReactNode;
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,11 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "@fontsource/poppins/300.css";
-import "@fontsource/poppins/400.css";
-import "@fontsource/poppins/500.css";
-import "@fontsource/poppins/600.css";
-import "@fontsource/poppins/700.css";
+
 import { AppProvider } from "@/features/misc/components/AppProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
