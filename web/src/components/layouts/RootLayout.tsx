7b3be793fa90a76f05d8bb744476fad96d4214ac
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-10-08 23:22:26 +0800

    refactor(web): removed sidebar; used dropdown menu instead

--- web/src/components/layouts/RootLayout.tsx
+++ web/src/components/layouts/RootLayout.tsx
@@ -1,25 +1,16 @@
 import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router-dom";
-import { useToggle } from "@/features/misc/hooks/useToggle";
-import { Sidebar } from "@/components/layouts/Sidebar";
 import { Header } from "@/components/layouts/Header";
-import { Backdrop } from "@/components/layouts/Backdrop";
 
 export const RootLayout = () => {
-  const { close, open, isOpen } = useToggle();
-
   return (
     <>
-      <div id="root_layout" className="flex h-screen">
-        <Sidebar isOpen={isOpen} close={close} />
-        <div className="flex-1">
-          <Header onMenuClick={open} />
-          <main className="min-h-[calc(100vh-64px)] p-4">
-            <Outlet />
-          </main>
-        </div>
+      <div id="root_layout" className="relative mx-auto h-screen max-w-md bg-background text-textSecondary shadow">
+        <Header />
+        <main className="h-[calc(100vh-64px)] scroll-m-2 overflow-y-auto p-6">
+          <Outlet />
+        </main>
       </div>
-      <Backdrop isOpen={isOpen} onClick={close} />
       <Toaster position="bottom-center" />
     </>
   );
