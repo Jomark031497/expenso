812ffcd1d010257b8dc1655cf90ef2855ab4615a
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-10-22 08:12:05 +0800

    feat: added auth providers discord and github

--- api/src/domains/auth/auth.controller.ts
+++ api/src/domains/auth/auth.controller.ts
@@ -1,28 +1,17 @@
 import type { Request, Response, NextFunction } from "express";
 import * as authService from "./auth.service.js";
-import { AppError } from "../../utils/appError.js";
 import { generateState, OAuth2RequestError } from "arctic";
 import { serializeCookie, parseCookies } from "oslo/cookie";
 import { envs } from "../../config/env.js";
 import { getUser, createUser } from "../users/users.service.js";
 import type { DiscordUser, GitHubUser } from "./auth.utils.js";
-import {
-  generateSessionToken,
-  createSession,
-  setSessionTokenCookie,
-  deleteSessionTokenCookie,
-  github,
-  discord,
-} from "./auth.utils.js";
+import { deleteSessionTokenCookie, github, discord, createSessionAndSetCookie } from "./auth.utils.js";
+import { AppError } from "../../utils/appError.js";
 
 export const loginUserHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const user = await authService.loginUser(req.body);
-
-    const token = generateSessionToken();
-    const session = await createSession(token, user.id);
-    setSessionTokenCookie(res, token, session.expiresAt);
-
+    await createSessionAndSetCookie(res, user.id);
     return res.status(200).json(user);
   } catch (error) {
     return next(error);
@@ -32,11 +21,7 @@ export const loginUserHandler = async (req: Request, res: Response, next: NextFu
 export const signUpUserHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const user = await authService.signUpUser(req.body);
-
-    const token = generateSessionToken();
-    const session = await createSession(token, user.id);
-    setSessionTokenCookie(res, token, session.expiresAt);
-
+    await createSessionAndSetCookie(res, user.id);
     return res.status(201).json(user);
   } catch (error) {
     return next(error);
@@ -45,11 +30,7 @@ export const signUpUserHandler = async (req: Request, res: Response, next: NextF
 
 export const getAuthenticatedUserHandler = async (_req: Request, res: Response, next: NextFunction) => {
   try {
-    const user = res.locals.user;
-    if (!user) throw new AppError(403, "forbidden: no session found");
-
-    const data = await authService.getAuthenticatedUser(user.id);
-
+    const data = await authService.getAuthenticatedUser(res.locals.user.id);
     return res.status(200).json(data);
   } catch (error) {
     return next(error);
@@ -58,10 +39,6 @@ export const getAuthenticatedUserHandler = async (_req: Request, res: Response, 
 
 export const logoutUserHandler = async (_req: Request, res: Response, next: NextFunction) => {
   try {
-    const session = res.locals.session;
-
-    if (!session) throw new AppError(403, "forbidden: no session found");
-
     deleteSessionTokenCookie(res);
     return res.status(200).json({ message: "logout success" });
   } catch (error) {
@@ -98,27 +75,21 @@ export const githubCallbackHandler = async (req: Request, res: Response, next: N
     const { code, state } = req.query;
     const storedState = parseCookies(req.headers.cookie ?? "").get("github_oauth_state");
 
-    if (!code || !state || !storedState || state !== storedState) {
+    if (!code || !state || !storedState || state !== storedState)
       return res.status(400).json({ error: "Invalid OAuth state or code" });
-    }
 
     const tokens = await github.validateAuthorizationCode(code.toString());
     const githubUserResponse = await fetch("https://api.github.com/user", {
       headers: { Authorization: `Bearer ${tokens.accessToken}` },
     });
 
-    if (!githubUserResponse.ok) {
-      throw new Error("Failed to fetch GitHub user");
-    }
+    if (!githubUserResponse.ok) throw new AppError(400, "Failed to fetch GitHub user");
 
     const githubUser = (await githubUserResponse.json()) as GitHubUser;
     const existingUser = await getUser("githubId", githubUser.id, { returnError: false });
 
     if (existingUser) {
-      const token = generateSessionToken();
-      const session = await createSession(token, existingUser.id);
-      setSessionTokenCookie(res, token, session.expiresAt);
-
+      await createSessionAndSetCookie(res, existingUser.id);
       return res.status(200).redirect(envs.CLIENT_URL);
     }
 
@@ -128,10 +99,7 @@ export const githubCallbackHandler = async (req: Request, res: Response, next: N
       email: githubUser.email,
     });
 
-    const token = generateSessionToken();
-    const session = await createSession(token, createdUser.id);
-    setSessionTokenCookie(res, token, session.expiresAt);
-
+    await createSessionAndSetCookie(res, createdUser.id);
     return res.redirect(envs.CLIENT_URL);
   } catch (error) {
     if (error instanceof OAuth2RequestError && error.message === "bad_verification_code") {
@@ -148,18 +116,18 @@ export const initiateDiscordLoginHandler = async (_req: Request, res: Response, 
       scopes: ["identify", "email"],
     });
 
-    // .appendHeader(
-    //   "Set-Cookie",
-    //   serializeCookie("github_oauth_state", state, {
-    //     secure: process.env.NODE_ENV === "production", // set to false in localhost
-    //     path: "/",
-    //     httpOnly: true,
-    //     maxAge: 60 * 10, // 10 min
-    //     sameSite: "lax",
-    //   }),
-    // )
-
-    return res.json({ url: url.toString() });
+    return res
+      .appendHeader(
+        "Set-Cookie",
+        serializeCookie("discord_oauth_state", state, {
+          secure: process.env.NODE_ENV === "production", // set to false in localhost
+          path: "/",
+          httpOnly: true,
+          maxAge: 60 * 10, // 10 min
+          sameSite: "lax",
+        }),
+      )
+      .json({ url: url.toString() });
   } catch (error) {
     return next(error);
   }
@@ -167,12 +135,11 @@ export const initiateDiscordLoginHandler = async (_req: Request, res: Response, 
 
 export const discordCallbackHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { code } = req.query;
-    // const storedState = parseCookies(req.headers.cookie ?? "").get("github_oauth_state");
+    const { code, state } = req.query;
+    const storedState = parseCookies(req.headers.cookie ?? "").get("discord_oauth_state");
 
-    if (!code) {
-      return res.status(400).json({ error: "Invalid OAuth code" });
-    }
+    if (!code || !state || !storedState || state !== storedState)
+      return res.status(400).json({ error: "Invalid OAuth state or code" });
 
     const tokens = await discord.validateAuthorizationCode(code.toString());
     const accessToken = tokens.accessToken;
@@ -181,19 +148,13 @@ export const discordCallbackHandler = async (req: Request, res: Response, next: 
       headers: { Authorization: `Bearer ${accessToken}` },
     });
 
-    if (!discordUserResponse.ok) {
-      throw new Error("Failed to fetch Discord user");
-    }
+    if (!discordUserResponse.ok) throw new AppError(400, "Failed to fetch Discord user");
 
     const discordUser = (await discordUserResponse.json()) as DiscordUser;
-
     const existingUser = await getUser("discordId", discordUser.id, { returnError: false });
 
     if (existingUser) {
-      const token = generateSessionToken();
-      const session = await createSession(token, existingUser.id);
-      setSessionTokenCookie(res, token, session.expiresAt);
-
+      await createSessionAndSetCookie(res, existingUser.id);
       return res.status(200).redirect(envs.CLIENT_URL);
     }
 
@@ -203,9 +164,7 @@ export const discordCallbackHandler = async (req: Request, res: Response, next: 
       email: discordUser.email,
     });
 
-    const token = generateSessionToken();
-    const session = await createSession(token, createdUser.id);
-    setSessionTokenCookie(res, token, session.expiresAt);
+    await createSessionAndSetCookie(res, createdUser.id);
 
     return res.redirect(envs.CLIENT_URL);
   } catch (error) {
