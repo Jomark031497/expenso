a0d6c29051066cb20ddfbfd3c0b44ef060aa3f94
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-11-03 16:35:01 +0800

    refactor: removed redundant comments

--- packages/api/src/utils/appError.ts
+++ packages/api/src/utils/appError.ts
@@ -2,22 +2,15 @@ export class AppError extends Error {
   statusCode: number;
   errors: Record<string, unknown>;
 
-  constructor(
-    statusCode: number = 500, // Default status code to 500
-    message: string,
-    errors: Record<string, unknown> = {}, // Default errors to empty object
-    stack = "",
-  ) {
+  constructor(statusCode: number = 500, message: string, errors: Record<string, unknown> = {}, stack = "") {
     super(message);
     this.statusCode = statusCode;
     this.errors = errors;
 
-    // Validate the status code is within HTTP range (100-599)
     if (statusCode < 100 || statusCode > 599) {
       throw new Error(`Invalid status code: ${statusCode}`);
     }
 
-    // Capture the stack trace if it's not provided
     if (stack) {
       this.stack = stack;
     } else {
--- packages/api/src/utils/shutdownServer.ts
+++ packages/api/src/utils/shutdownServer.ts
@@ -2,22 +2,18 @@ import type { Server } from "http";
 import { closeDbConnection } from "../db/dbInstance.js";
 import { logger } from "./logger.js";
 
-// Graceful shutdown with a timeout and DB close
 export async function shutdown(signal: string, server: Server) {
   logger.info(`${signal} signal received: closing HTTP server`);
 
-  // Set a timeout to forcefully exit if shutdown takes too long
   const timeout = setTimeout(() => {
     logger.error("Force shutting down server due to timeout.");
     process.exit(1);
-  }, 30_000); // 30 seconds timeout
+  }, 30_000);
 
-  // Close the server first
   server.close(async () => {
     clearTimeout(timeout);
     logger.info("HTTP server closed");
 
-    // Close the database connection
     try {
       await closeDbConnection();
       logger.info("Database connection closed.");
