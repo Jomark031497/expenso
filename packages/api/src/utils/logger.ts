74f1f96ff3501ac88b4167b898772b6235ce1a2e
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-11-03 16:32:29 +0800

    refactor: prettified logger:

--- packages/api/src/utils/logger.ts
+++ packages/api/src/utils/logger.ts
@@ -5,15 +5,11 @@ export const logger = pino({
   base: {
     pid: false,
   },
-  level: process.env.NODE_ENV === "test" ? "silent" : "info",
-  ...(process.env.NODE_ENV === "development" && {
-    transport: {
-      target: "pino-pretty",
-      options: {
-        colorize: true,
-      },
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
     },
-  }),
-
+  },
   timestamp: () => `,"time":"${format(new Date(), "hh:mm:ss aa | MMM-dd-yyyy")}"`,
 });
