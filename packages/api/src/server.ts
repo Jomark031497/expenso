50001251434fa7c1ba6100f311ee3922f00baaec
Jomark Pangan <jomarkrazonpangan@gmail.com>
2024-11-03 16:28:43 +0800

    refactor: created shutdownServer utility function

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,7 +1,7 @@
 import { logger } from "./utils/logger.js";
 import { createApp } from "./app.js";
-import { closeDbConnection } from "./db/dbInstance.js"; // Import closeDbConnection
 import { envs } from "./config/env.js";
+import { shutdown } from "./utils/shutdownServer.js";
 
 const main = async () => {
   const app = createApp();
@@ -12,36 +12,9 @@ const main = async () => {
     });
   });
 
-  // Graceful shutdown with a timeout and DB close
-  async function shutdown(signal: string) {
-    logger.info(`${signal} signal received: closing HTTP server`);
-
-    // Set a timeout to forcefully exit if shutdown takes too long
-    const timeout = setTimeout(() => {
-      logger.error("Force shutting down server due to timeout.");
-      process.exit(1);
-    }, 30_000); // 30 seconds timeout
-
-    // Close the server first
-    server.close(async () => {
-      clearTimeout(timeout);
-      logger.info("HTTP server closed");
-
-      // Close the database connection
-      try {
-        await closeDbConnection();
-        logger.info("Database connection closed.");
-        process.exit(0);
-      } catch (err) {
-        logger.error("Error closing database connection:", err);
-        process.exit(1);
-      }
-    });
-  }
-
   // Capture termination signals
-  process.on("SIGINT", () => shutdown("SIGINT"));
-  process.on("SIGTERM", () => shutdown("SIGTERM"));
+  process.on("SIGINT", () => shutdown("SIGINT", server));
+  process.on("SIGTERM", () => shutdown("SIGTERM", server));
 
   // Handle unhandled promise rejections
   process.on("unhandledRejection", (reason, promise) => {
